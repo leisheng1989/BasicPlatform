337abc054787bfa244d912c51a5d3266e827210f
Just Do It <527891344@qq.com>
2017-03-07 15:33:50 +0800

    安装uninstall.sh脚本

--- target/install.sh
+++ target/install.sh
@@ -10,6 +10,7 @@ ROOT_PATCH=`pwd`
 mkdir -p ${HOME}/.${PROJECT_NAME}/logs && \
 sudo mkdir -p ${INSTALL_PREFIX}/bin && \
 sudo mkdir -p ${INSTALL_PREFIX}/configs && \
+sudo cp -Ra ${ROOT_PATCH}/uninstall.sh ${INSTALL_PREFIX}/uninstall.sh && \
 sudo cp -Ra ${ROOT_PATCH}/bin/* ${INSTALL_PREFIX}/bin && \
 sudo cp -Ra ${ROOT_PATCH}/configs/* ${INSTALL_PREFIX}/configs && \
 sudo ln -sf ${INSTALL_PREFIX}/bin/${PROJECT_NAME} /usr/bin/${PROJECT_NAME}
