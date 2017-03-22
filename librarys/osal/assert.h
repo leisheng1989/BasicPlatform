2c563dfa3aa52392e05b46d3c2db547336a3c087
Just Do It <527891344@qq.com>
2017-03-22 14:00:50 +0800

    assert断言，终止程序运行

--- librarys/osal/assert.h
+++ librarys/osal/assert.h
@@ -1,12 +1,20 @@
 #ifndef __ASSERT__H__
 #define __ASSERT__H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #undef assert
 #ifdef NDEBUG
 #define assert(expr) ((void) 0)
 #else
 #define assert(expr) \
-    ((expr) ? ((void) 0) : SysFatalTrace("!!ASSERTION FAILED: \"" #expr "\""))\
+    ((expr) ? ((void) 0) : (SysFatalTrace("!!ASSERTION FAILED: \"" #expr "\""); abort()))\
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif /* __ASSERT__H__ */
